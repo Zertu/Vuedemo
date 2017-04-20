d3a3727d071e9b8a1719e28b391d2fe3add7c6dd
zertu <290055513@qq.com>
2017-04-20 19:45:28 +0800

    添加服务端

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import * as nodejieba from 'nodejieba'
-
 import App from './App'
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
