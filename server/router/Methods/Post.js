1707bf3abe9aa03cdefad106fdec2c30c38c7661
root <290055513@qq.com>
2017-04-23 23:07:27 +0800

    Changes to be committed: 	modified:   server/package.json 	modified:   server/router/Methods/Post.js  Untracked files: 	server/.sudo.swp

--- server/router/Methods/Post.js
+++ server/router/Methods/Post.js
@@ -1,15 +1,24 @@
+const nodejieba = require("nodejieba")
 exports = module.exports= async function (ctx) {    
     const req = ctx.request
     switch (req.url) {
         case '/login':
             login(req, ctx.response)            
-        default:
+	case '/fenci':
+		fenci(req,ctx.response)        
+default:
         break
     }
     return ctx.response
 }
 
+async function fenci(req,res){
+	const result = nodejieba.cut(req.query['sentence'])
+	res.body=result
+
+}
 
 async function login(req, res) {
      
+
 }
