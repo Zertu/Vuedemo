1f7f2257d71fd3959049ea2e2a6918613bde9fd4
zertu <290055513@qq.com>
2017-05-23 15:27:19 +0800

    去除console

--- router/weiboData/index.js
+++ router/weiboData/index.js
@@ -2,7 +2,6 @@ routers = require('koa-router'),
 router = routers(), {stewpot} = require('../../util/')
 router.get('/getWeiboData', async function (next) {
   const query = this.request.query
-  console.log(query)
   this.response.body = await stewpot.findAndCountAll({limit: query.num, offset: query.pagenum})
 })
 module.exports = router
