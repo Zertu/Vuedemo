25c2965c152fd3ff48b09393c27e1f73f48816b6
zertu <290055513@qq.com>
2017-05-26 09:31:43 +0800

    排序bug

--- router/weiboData/index.js
+++ router/weiboData/index.js
@@ -26,12 +26,12 @@ router.get('/eleme', async function (next) {
   const query = this.request.query,
   pageSize=query.num-0,
   idx=query.pagenum-0
-  this.response.body = await eleme.findAndCountAll({limit: pageSize , offset:idx*pageSize-pageSize,order:'create_time asc'})
+  this.response.body = await eleme.findAndCountAll({limit: pageSize , offset:idx*pageSize-pageSize})
 })
 router.get('/beef', async function (next) {
   const query = this.request.query,
   pageSize=query.num-0,
   idx=query.pagenum-0
-  this.response.body = await beef.findAndCountAll({limit: pageSize , offset:idx*pageSize-pageSize,order:'create_time asc'})
+  this.response.body = await beef.findAndCountAll({limit: pageSize , offset:idx*pageSize-pageSize})
 })
 module.exports = router
